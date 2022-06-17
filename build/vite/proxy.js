731d73f7382837c4235b9e94bcdb03624e286c6b
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-06-17 18:17:44 +0800

    update

--- build/vite/proxy.js
+++ build/vite/proxy.js
@@ -13,4 +13,5 @@ export const createProxy = (list = []) => {
       ...(isHttps ? { secure: false } : {})
     }
   }
+  return res
 }
---
+++ src/hooks/web/useMessage.js
@@ -0,0 +1,8 @@
+import { ElDialog, ElMessage, ElNotification } from 'element-plus'
+
+import { useI18n } from './useI18n.js'
+import { isString } from '@/utils/is.js'
+
+export const useMessage = () => {
+
+}
