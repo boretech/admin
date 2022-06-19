69329aac63586b3f5045438e924b02753b683ff8
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-06-19 23:27:07 +0800

    feat: start mock

---
+++ mock/_createProductionServer.js
@@ -0,0 +1,18 @@
+import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
+
+const modules = import.meta.globEager('./**/*.js')
+
+let mockModules = []
+Object.keys(modules).forEach((key) => {
+  if (key.includes('/_')) {
+    return
+  }
+  mockModules.push(...modules[key].default)
+})
+
+/**
+ * Used in a production environment. Need to manually import all modules
+ */
+export const setupProdMockServer = () => {
+  createProdMockServer(mockModules)
+}
