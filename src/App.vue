64610f72e6c66c8300d7430ace69141648ab078d
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-06-27 16:12:05 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <router-view />
+  <ElConfigProvider :locale="getAntdLocale">
+    <AppProvider>
+      <RouterView />
+    </AppProvider>
+  </ElConfigProvider>
 </template>
 
-<script>
-export default {
-  name: 'home'
-}
-</script>
-
 <script setup>
-
+import { ElConfigProvider } from 'element-plus'
+import { AppProvider } from '@/components/Application/index.js'
+import { useTitle } from '@/hooks/web/useTitle.js'
+import { useLocale } from '@/locales/useLocale.js'
+import 'dayjs/locale/zh-cn'
+// support Multi-language
+const { getAntdLocale } = useLocale()
+// Listening to page changes and dynamically changing site titles
+useTitle()
 </script>
