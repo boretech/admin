940d49926ec1b16d35401c91264ccd1728ab99ad
xiaofuyesnew <xiaofuyesnew@qq.com>
2022-07-18 20:42:01 +0800

    feat: icons page

---
+++ src/router/routes/modules/demo.js
@@ -0,0 +1,25 @@
+import Layout from '@/layout/index.vue'
+
+export const promotionRoute = {
+  path: '/promotion',
+  name: 'Promotion',
+  component: Layout,
+  redirect: '/promotion/icons',
+  meta: {
+    title: '功能'
+  },
+  children: [
+    {
+      path: 'icons',
+      name: 'Icons',
+      component: () => import('@/views/promotion/Icons.vue'),
+      meta: {
+        title: '图标'
+      }
+    }
+  ]
+}
+
+export default {
+  promotionRoute
+}
